8ca217eacd08b33abe418b58d293646d9c0afbbb
chisato410 <it242243@trident.ac.jp>
2025-12-12 11:04:30 +0900

    ルーティングの修正

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Closet from "./pages/Closet";
 import ItemDetail from "./pages/ItemDetail";
-import Upload from "./pages/Upload";
+import AddItem from "./pages/AddItem";
+import SelectCategory from "./pages/SelectCategory";
+import SelectType from "./pages/SelectType";
 import MemoList from "./pages/MemoList";
+import MemoNew from "./pages/MemoNew";
+import MemoFolder from "./pages/MemoFolder";
+import Settings from "./pages/Settings";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/closet" element={<Closet />} />
-        <Route path="/item/:id" element={<ItemDetail />} />
-        <Route path="/upload" element={<Upload />} />
+        <Route path="/closet/:id" element={<ItemDetail />} />
+        <Route path="/add" element={<AddItem />} />
+        <Route path="/select/category" element={<SelectCategory />} />
+        <Route path="/select/type" element={<SelectType />} />
         <Route path="/memo" element={<MemoList />} />
+        <Route path="/memo/new" element={<MemoNew />} />
+        <Route path="/memo/folder" element={<MemoFolder />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
